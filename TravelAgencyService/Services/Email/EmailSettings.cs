b30a18a9138c97025b5e3354fffccfb733a6958f
nevoitzhak1 <nevoit@ac.sce.ac.il>
2026-01-07 15:41:42 +0200

    reminders update
    added trip reminders to mail

---
+++ TravelAgencyService/Services/Email/EmailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelAgencyService.Services.Email
+{
+    public class EmailSettings
+    {
+        public string FromEmail { get; set; } = "";
+        public string FromName { get; set; } = "";
+        public string SmtpHost { get; set; } = "";
+        public int SmtpPort { get; set; } = 587;
+        public string SmtpUser { get; set; } = "";
+        public string SmtpPass { get; set; } = "";
+    }
+}
